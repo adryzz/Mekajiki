e6bda33d5ba7970c8fb25d660a1f7f2fee3892da
Adryzz <46694241+adryzz@users.noreply.github.com>
2022-02-03 19:40:21 +0100

    add diag

---
+++ Server/Diagnostics/ServerManager.cs
@@ -0,0 +1,22 @@
+using NodaTime;
+using CircularBuffer;
+
+namespace Mekajiki.Server.Diagnostics;
+
+public class ServerManager
+{
+    public const int BufferSize = 64;
+
+    public Duration Uptime { get; private set; }
+    public CircularBuffer<(ulong, ulong)> MemoryInfo { get; } = new(BufferSize);
+
+    public CircularBuffer<int> Load { get; } = new(BufferSize);
+
+    public void Update()
+    {
+        var data = systemInfo.sysinfo();
+        Uptime = Duration.FromSeconds(data.uptime);
+        MemoryInfo.PushFront((data.totalram, data.totalram - data.freeram));
+        Load.PushFront(0);
+    }
+}
